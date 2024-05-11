7e933f4c5cb613a809b208ef4948ee70aea068d3
Vadim <164755052+VadimDevelopment2432@users.noreply.github.com>
2024-05-11 18:37:17 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	app.setGlobalPrefix('api')
 	app.enableCors()
 	await app.listen(3000)
 }
